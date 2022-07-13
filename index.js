1f1dacfa99bac2c72556a8cf5fb2d479eca2f345
Eric Alvarez <alvarezaeric592@gmail.com>
2022-07-12 19:57:56 -0500

    agrego boton de limpiar

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const botton = document.getElementById("calc");
 const mostrar = document.getElementById("show-result");
-const add = document.getElementById("addNumber")
-
+const add = document.getElementById("addNumber");
+const mostrarNumero = document.getElementById("show-numbers");
+const limpiar = document.getElementById("clean");
 // llamada a la funcion calcular 
 
 botton.addEventListener("click",calcular);
 add.addEventListener("click",addNumber);
+limpiar.addEventListener("click",clear)
 
 
 const numeros = [];
@@ -13,6 +15,9 @@ const numeros = [];
 function addNumber () {
     const valores = parseFloat(document.getElementById("Prom-value").value);
     numeros.push(valores);
+
+    mostrarNumero.innerHTML=numeros;
+
     console.log(numeros)
     
 }
@@ -24,5 +29,15 @@ function calcular(){
     
     mostrar.innerHTML =resultado;
     
+
+}
+
+function clear(){
+
+    mostrar.innerHTML = 00;
+    numeros.pop()
+    mostrarNumero.innerHTML = numeros;
+    console.log(numeros);
+
 
 }
